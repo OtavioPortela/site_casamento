2f0028736a80ac5b1fdcca6a85c830e8416e4e48
OtavioPortela <81724162+OtavioPortela@users.noreply.github.com>
2024-06-26 19:15:23 -0300

    alteração config

--- migrate.py
+++ migrate.py
@@ -1,27 +1,16 @@
 from app import app, db
-import sqlite3
+from flask_sqlalchemy import SQLAlchemy
 
 def add_column():
-    # Conectar ao banco de dados diretamente
-    conn = sqlite3.connect('instance/site.db')
-    cursor = conn.cursor()
+    db = SQLAlchemy(app)
 
-    # Verificar se a tabela 'present' existe
-    cursor.execute("SELECT name FROM sqlite_master WHERE type='table' AND name='present'")
-    table_exists = cursor.fetchone()
+    # Verificar se a coluna 'image_file' já existe
+    columns = db.engine.execute("SELECT column_name FROM information_schema.columns WHERE table_name = 'present' AND column_name = 'image_file'")
+    column_exists = columns.fetchone()
 
-    if table_exists:
-        # Verificar se a coluna 'image_file' já existe
-        cursor.execute("PRAGMA table_info(present)")
-        columns = [column[1] for column in cursor.fetchall()]
-
-        if 'image_file' not in columns:
-            # Adicionar a nova coluna
-            cursor.execute('ALTER TABLE present ADD COLUMN image_file VARCHAR(200) NOT NULL DEFAULT ""')
-
-    # Salvar as alterações e fechar a conexão
-    conn.commit()
-    conn.close()
+    if not column_exists:
+        # Adicionar a nova coluna
+        db.engine.execute('ALTER TABLE present ADD COLUMN image_file VARCHAR(200) NOT NULL DEFAULT ""')
 
 with app.app_context():
     add_column()
