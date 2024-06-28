c5e1a715446727343e5c6f62a841a0dab00798eb
OtavioPortela <81724162+OtavioPortela@users.noreply.github.com>
2024-06-27 21:55:57 -0300

    Correções e ajustes finais para deployment no Heroku

--- config.py
+++ config.py
@@ -1,21 +1,8 @@
 import os
 
 class Config:
-    SECRET_KEY = os.environ.get('SECRET_KEY', 'fallback_secret_key')
+    SECRET_KEY = os.environ.get('SECRET_KEY', 'your_secret_key')
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL').replace("postgres://", "postgresql://")
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-    UPLOAD_FOLDER = os.environ.get('UPLOAD_FOLDER', 'uploads/')  # Adiciona esta linha
-
-
-class DevelopmentConfig(Config):
-    DEBUG = True
-
-class ProductionConfig(Config):
-    DEBUG = False
-
-# Ensure the DATABASE_URL environment variable is set
-os.environ['DATABASE_URL'] = Config.SQLALCHEMY_DATABASE_URI
-
-
-
-
+    UPLOAD_FOLDER = 'uploads/'
+    MAX_CONTENT_PATH = 16 * 1024 * 1024  # 16 MB
