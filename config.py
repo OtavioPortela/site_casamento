a787eab404aeee941272ca3becdc1842103885e9
OtavioPortela <81724162+OtavioPortela@users.noreply.github.com>
2024-06-27 21:22:42 -0300

    Corrigido o URI do banco de dados para usar 'postgresql' em vez de 'postgres'

--- config.py
+++ config.py
@@ -1,10 +1,10 @@
 import os
 
 class Config:
-    # Define the PostgreSQL URI directly
-    SQLALCHEMY_DATABASE_URI = 'postgres://ubksl4tqlamln4:p1c21324f05453e39b2141da690c0b6b22f4d87c6f6e3af6a5c16f4fcb87bc1f1@cd5gks8n4kb20g.cluster-czrs8kj4isg7.us-east-1.rds.amazonaws.com:5432/d5qaspp2blgbc0'
+    SECRET_KEY = os.environ.get('SECRET_KEY') or 'sua_chave_secreta_super_segura'
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL').replace("postgres://", "postgresql://")
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-    UPLOAD_FOLDER = 'uploads/'
+    UPLOAD_FOLDER = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'static', 'uploads')
 
 class DevelopmentConfig(Config):
     DEBUG = True
