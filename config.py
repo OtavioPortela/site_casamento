556f5eb74e9b1b81ab46e5c0d2f0f644388a7cb2
OtavioPortela <81724162+OtavioPortela@users.noreply.github.com>
2024-06-27 21:17:46 -0300

    Update config to use PostgreSQL

--- config.py
+++ config.py
@@ -1,11 +1,10 @@
 import os
 
 class Config:
-    # Use a variável de ambiente DATABASE_URL se estiver definida, caso contrário use o SQLite para desenvolvimento
-    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL', 'sqlite:///site.db').replace("postgres://", "postgresql://", 1)
+    # Define the PostgreSQL URI directly
+    SQLALCHEMY_DATABASE_URI = 'postgres://ubksl4tqlamln4:p1c21324f05453e39b2141da690c0b6b22f4d87c6f6e3af6a5c16f4fcb87bc1f1@cd5gks8n4kb20g.cluster-czrs8kj4isg7.us-east-1.rds.amazonaws.com:5432/d5qaspp2blgbc0'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-    UPLOAD_FOLDER = os.path.join(os.getcwd(), 'app/static/uploads')
-    MAX_CONTENT_PATH = 16 * 1024 * 1024  # 16 MB
+    UPLOAD_FOLDER = 'uploads/'
 
 class DevelopmentConfig(Config):
     DEBUG = True
@@ -13,5 +12,5 @@ class DevelopmentConfig(Config):
 class ProductionConfig(Config):
     DEBUG = False
 
-# Adicione a URL do PostgreSQL diretamente aqui para garantir que está sendo usada
-os.environ['DATABASE_URL'] = 'postgresql://ubksl4tqlamln4:p1c21324f05453e39b2141da690c0b6b22f4d87c6f6e3af6a5c16f4fcb87bc1f1@cd5gks8n4kb20g.cluster-czrs8kj4isg7.us-east-1.rds.amazonaws.com:5432/d5qaspp2blgbc0'
+# Ensure the DATABASE_URL environment variable is set
+os.environ['DATABASE_URL'] = Config.SQLALCHEMY_DATABASE_URI
